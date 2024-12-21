d7fde50bf985e724103b11bf18fe7f0ea3df59dd
luisaraujoc <luislk_@outlook.com>
2024-12-21 13:49:01 -0300

    Resetando o projeto.
    O projeto precisa ser resetado. Pela forma que foi construido, ele tá equivocado, o banco vai ser bem mais complexo. Vamos começar do jeito correto.

--- src/app.js
+++ src/app.js
@@ -2,15 +2,19 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const routes = require("./routes");
-const userRoutes = require("./routes/userRoutes");
-const adressRoutes = require("./routes/adressRoutes");
-const personRoutes = require("./routes/personRoutes");
-const storeRoutes = require("./routes/storeRoutes");
-const serviceRoutes = require("./routes/servicesRoutes");
-const sequelize = require("./config/database");
 
 // Express
 const app = express();
+
+// Sequelize Sync Config
+sequelize.sync({
+    force: true,
+    alter: true,
+}).then(() => {
+    console.log("Tabelas sincronizadas");
+}).catch((error) => {
+    console.error("Erro ao sincronizar tabelas:", error);
+});
 
 // App Use
 app.use(cors());
@@ -19,25 +23,20 @@ app.use(express.json());
 // Routes
 app.use(routes);
 
-// User Routes
-app.use("/api/users", userRoutes);
-app.use("/api/adress", adressRoutes);
-app.use("/api/persons", personRoutes);
-app.use("/api/stores", storeRoutes);
-app.use("/api/services", serviceRoutes);
 
-sequelize.sync({force: true});
-
+app.get("/", (req, res) => {
+    res.json({message: "Olá, mundo!"});
+});
 
 // Erro de rotas
 app.use((req, res, next) => {
-  res.status(404).json({ message: "Rota não encontrada" });
+    res.status(404).json({message: "Rota não encontrada"});
 });
 
 // Middleware de erro geral
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: "Erro interno do servidor" });
+    console.error(err.stack);
+    res.status(500).json({message: "Erro interno do servidor"});
 });
 
 module.exports = app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,1 @@
-const express = require('express');
-// const userRoutes = require('./userRoutes'); // Importa as rotas de usuários
-
-const router = express.Router();
-
-// Hello World
-router.get('/', (req, res) => {
-  res.json({ message: 'Hello World!' });
-});
-
-
-module.exports = router;
+// reset
